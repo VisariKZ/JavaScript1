8331cc1176ef7c48ff2a0d185544039ffc33afcc
Matías Verón <matiasiveron@gmail.com>
2025-06-29 17:48:31 -0300

    Segundo commit

---
+++ Semana-1/main.js
@@ -0,0 +1,29 @@
+console.log("Hola mundo")
+
+//alert("Hola chicos")
+
+let nombre = "Matias"
+
+console.log(nombre)
+
+//alert(nombre)
+
+//let edad = prompt("Cuantos años tenes?")
+
+//console.log(Number(edad))    
+//console.log(typeof edad) //number
+
+// edad = true
+
+// console.log(edad)
+// console.log(typeof edad) //boolean
+
+let quiereUnHelado = confirm("¿Quiere helado de vainilla?")
+
+console.log(quiereUnHelado)
+
+let apellido = prompt("¿Cual es tu apellido?")
+
+console.log(apellido)
+
+console.log( 0.1 + 0.2 )
